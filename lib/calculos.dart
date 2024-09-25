52d66e3c0faab15d5af836dafc048b25ae2820cd
Eduardo8006 <eduardodesouza8006@gmail.com>
2024-09-25 16:28:51 -0400

    Outros calculos

--- lib/calculos.dart
+++ lib/calculos.dart
@@ -4,7 +4,24 @@ Matrix2d m2d = Matrix2d();
 
 List multiplicacao(List matriz1, List matriz2) {
   List resultado = m2d.dot(matriz1, matriz2);
-  print(resultado);
+
+  return resultado;
+}
+
+List divisao(List matriz1, List matriz2) {
+  List resultado = m2d.division(matriz1, matriz2);
+
+  return resultado;
+}
+
+List subtracao(List matriz1, List matriz2) {
+  List resultado = m2d.subtraction(matriz1, matriz2);
+
+  return resultado;
+}
+
+List adicao(List matriz1, List matriz2) {
+  List resultado = m2d.addition(matriz1, matriz2);
 
   return resultado;
 }
