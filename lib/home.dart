708445e6a095f2fbeb9badc19d1793ff1ea9a65b
Júlio <juliocesarsabaini55@gmail.com>
2024-09-25 20:15:31 -0400

    principal meio pronto

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:operacaomatriz/calculos.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -9,37 +10,99 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List a = [
-    [1, 2, 3],
-    [2, 3, 4],
-    [2, 3, 4]
-  ];
-  List b = [
-    [67, 5, 3],
-    [13, 3, 4],
-    [8, 6, 5]
-  ];
-
-  @override
-  void initState() {
-    multiplicacao(a, b);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Calculadora de Matrizes"),
+        actions: <Widget>[
+          Text('substituir depois'),
+        ],
+        title: const Text("Matrizes"),
+        leading: Builder(
+          // Usando Builder para garantir o contexto correto
+          builder: (context) {
+            return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: Icon(
+                Icons.menu,
+                size: 30,
+              ),
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        // Barra lateral expansível
+        child: Container(
+          width:
+              MediaQuery.of(context).size.width * 0.5, // Ocupa metade da tela
+          color: Colors.grey[300],
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              // Alterando o tamanho da área azul no DrawerHeader
+              SizedBox(
+                height: 150, // Defina a altura desejada para o DrawerHeader
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.teal, // Cor azul
+                  ),
+                  child: Text(
+                    'Menu',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  margin: EdgeInsets.zero, // Remove a margem do DrawerHeader
+                  padding: EdgeInsets.zero, // Remove o padding do DrawerHeader
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Ação'),
+                onTap: () {
+                  Navigator.pop(context); // Fecha o drawer
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.remove),
+                title: Text('Subtração'),
+                onTap: () {
+                  Navigator.pop(context); // Fecha o drawer
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.close),
+                title: Text('multiplicação'),
+                onTap: () {
+                  Navigator.pop(context); // Fecha o drawer
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  FontAwesomeIcons.divide,
+                  size: 19,
+                ),
+                title: Text('Divisão'),
+                onTap: () {
+                  Navigator.pop(context); // Fecha o drawer
+                },
+              ),
+            ],
+          ),
+        ),
       ),
       body: Container(
+        color: Colors.blue,
         width: double.infinity,
         height: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextFormField(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[],
+          ),
         ),
       ),
     );
